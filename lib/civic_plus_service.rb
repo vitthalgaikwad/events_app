7b08c0f21b212718f8fd322054f22d3e91f4ba47
Vitthal Gaikwad <gaikwadvitthal04@gmail.com>
2019-04-16 13:19:46 +0530

    Add httparty gem and events api service lib

---
+++ lib/civic_plus_service.rb
@@ -0,0 +1,45 @@
+class CivicPlusService
+
+	include HTTParty
+  base_uri 'https://interview.cpdv.ninja'
+
+	def initialize(prefix)
+		@request_prefix = prefix
+		@auth_token = auth_token
+	end
+
+	def all_events(params={})
+		response = self.class.get(api_url('Events'), headers: headers, query: params)
+		response.parsed_response
+	end
+
+	def create_event(params={})
+		response = self.class.post(api_url('Events'), headers: headers.merge!({ 'Content-Type' => 'application/json' }), body: params.to_json)
+		response
+	end
+
+	def event_details(params={})
+		response = self.class.post(api_url("Events/#{params[:id]}"), headers: headers)
+		response.parsed_response
+	end
+
+	private
+
+	def headers
+		{
+			'Authorization' => "Bearer #{@auth_token}",
+			'Accept' => 'application/json'
+		}
+	end
+
+	def api_url(api)
+		"/#{@request_prefix}/api/#{api}"
+	end
+
+	def auth_token
+		response = self.class.post(api_url('Auth'),
+			query: { clientId: "48544448-65f9-43de-9d13-69c5c4b78038",
+			clientSecret: "cXLzWczX6VrUDTypx0JRgN/mtebqJ6sJxaHtyrFzTeE=" })
+		response.parsed_response['access_token']
+	end
+end
